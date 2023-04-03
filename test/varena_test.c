0bff588e28bc165422b789c2c31f6479631fa131
jakubtomsu <66876057+jakubtomsu@users.noreply.github.com>
2023-04-03 17:40:06 +0200

    Update varena_test.c

--- test/varena_test.c
+++ test/varena_test.c
@@ -7,10 +7,10 @@ UTEST(varena, common) {
     const int size = 1024 * 1024;
     VArena arena = varena_init(size);
     ASSERT_TRUE(varena_is_valid(arena));
-    ASSERT_EQ(varena._buf_len, size);
+    ASSERT_EQ(arena._buf_len, size);
     ASSERT_TRUE(varena_push(&arena, 1024));
-    ASSERT_EQ(varena.len, 1024);
-    ASSERT_EQ(varena._commited, 1024);
+    ASSERT_EQ(arena.len, 1024);
+    ASSERT_EQ(arena._commited, 1024);
     for(int i = 0; i < 10; i++) {
         ASSERT_TRUE(varena_push(&arena, vmem_get_page_size() * 2));
     }
